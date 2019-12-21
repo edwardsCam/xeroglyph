0e7cd6131a84d05514cdf8aba34bf14beb96b15d
Cameron Edwards <edwards.cameron.pc@gmail.com>
2019-12-20 19:47:19 -0700

    added a basic info modal

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
+Just doing some experimentation with the p5.js library.
+
 ## Patterns
 * Swirl: https://edwardscam.github.io/p5?pattern=swirl
 * Infinity Cycle: https://edwardscam.github.io/p5?pattern=infinity
 * Chipboard: https://edwardscam.github.io/p5?pattern=chipboard
 * Cityscape: https://edwardscam.github.io/p5?pattern=city
+
+## Help
+* Hit `i` to toggle an info panel.
+* Coming soon - knobs and tweakable parameters!
--- index.js
+++ index.js
@@ -1,31 +1,52 @@
-import * as _p5_ from 'p5'
 import React from 'react'
 import P5Wrapper from 'react-p5-wrapper'
 import ReactDOM from 'react-dom'
 
 import { board, city } from 'patterns/chipboard'
 import InfinityCycle from 'patterns/infinity-cycle'
-import Roots from 'patterns/roots'
 import Swirl from 'patterns/swirl'
 
-import AAA from 'patterns/exp'
+import HelpModal from 'components/helpModal'
+
+const defaultPattern = 'swirl'
+
+const patternMap = {
+  infinity: InfinityCycle,
+  swirl: Swirl,
+  chipboard: board,
+  city: city
+}
 
 const getPattern = () => {
   const patternParam = window.location.search.split('pattern=')
-  if (patternParam.length <= 1) return Swirl
-
-  const pattern = patternParam[1]
-  switch (pattern) {
-    case 'infinity': return InfinityCycle
-    case 'swirl': return Swirl
-    case 'chipboard': return board
-    case 'city': return city
-  }
+  return patternParam.length > 1 ? patternParam[1] : 'swirl'
 }
 
+const getSketch = () => patternMap[getPattern()]
+
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isShowingHelpModal: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', e => {
+      if (e.key == 'i') {
+        this.setState(prevState => ({ isShowingHelpModal: !prevState.isShowingHelpModal }))
+      }
+    })
+  }
+
   render() {
-    return <P5Wrapper sketch={getPattern()} />
+    return (
+      <div>
+        <P5Wrapper sketch={getSketch()} />
+        {this.state.isShowingHelpModal && <HelpModal pattern={getPattern()} />}
+      </div>
+    )
   }
 }
 
