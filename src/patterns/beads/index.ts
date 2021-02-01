27c0a6ef436e9a50c069aa22803c608e82c71557
Cameron Edwards <edwards.cameron.pc@gmail.com>
2021-01-31 22:41:20 -0700

    update beads pattern

--- src/patterns/beads/index.ts
+++ src/patterns/beads/index.ts
@@ -1,10 +1,12 @@
 import { init as initProps, getProp } from 'utils/propConfig.ts'
-import { interpolate, Point } from 'utils/math.ts'
+import { interpolate, coinToss } from 'utils/math.ts'
+import shuffle from 'utils/shuffle.ts'
 
 type Props = {
   n: number
   base: number
   variance: number
+  doShuffle: boolean
 }
 
 export default (s) => {
@@ -30,12 +32,16 @@ export default (s) => {
       default: 30,
       min: 0,
     },
+    shuffle: {
+      type: 'boolean',
+    },
   })
   const get = (prop: string) => getProp('beads', prop)
   const getProps = (): Props => ({
     n: get('n'),
     base: get('base'),
     variance: get('variance'),
+    doShuffle: get('shuffle'),
   })
 
   let last: Props | undefined
@@ -57,18 +63,36 @@ export default (s) => {
     }
 
     s.clear()
-    const { n, base, variance } = props
-    for (let r = 0; r < n; r++) {
-      for (let c = 0; c < n; c++) {
+    const { n, base, variance, doShuffle } = props
+    const cols: number[] = []
+    for (let c = 0; c < n; c++) {
+      cols.push(c)
+    }
+    ;(doShuffle ? shuffle(cols) : cols).forEach((c) => {
+      const inverted = coinToss()
+      for (
+        let r = inverted ? n - 1 : 0;
+        inverted ? r >= 0 : r < n;
+        inverted ? r-- : r++
+      ) {
         const x = interpolate([0, n - 1], [50, window.innerWidth - 50], c)
         const y = interpolate([0, n - 1], [50, window.innerHeight - 50], r)
         const noise = s.noise(x, y) * variance
         const radius = Math.max(0, base + noise)
 
-        // s.fill(interpolate([0, 1], [0, 30], Math.random()), interpolate([0, 1], [10, 50], Math.random()), 100)
+        s.fill(
+          50, //interpolate([0, 1], [0, 5], Math.random()),
+          interpolate([0, 1], [0, 8], Math.random()),
+          interpolate([0, 1], [92, 100], Math.random())
+        )
+        s.stroke(
+          0,
+          interpolate([0, 1], [0, 8], Math.random()),
+          interpolate([0, 1], [0, 8], Math.random())
+        )
         s.circle(x, y, radius)
       }
-    }
+    })
     last = props
   }
 }
